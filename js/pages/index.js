dc9149503872aee24883cba7ff810cab32d87ea1
Chun Sung Jin <chunsung@teach.cs.toronto.edu>
2019-03-04 20:39:05 -0500

    major changes done

---
+++ js/pages/index.js
@@ -0,0 +1,34 @@
+﻿$(function () {
+    //Widgets count
+    $('.count-to').countTo();
+
+    //Sales count to
+    $('.sales-count-to').countTo({
+        formatter: function (value, options) {
+            return '$' + value.toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, ' ').replace('.', ',');
+        }
+    });
+
+    initDonutChart();
+    initSparkline();
+});
+
+
+function initDonutChart() {
+    Morris.Donut({
+        element: 'donut_chart',
+        data: [{
+            label: 'Female',
+            value: 295
+        },
+        {
+            
+            label: 'Male',
+            value: 328
+        }],
+        colors: ['rgb(233, 0, 0)', 'rgb(0, 0, 233)'],
+        formatter: function (y) {
+            return y
+        }
+    });
+}
